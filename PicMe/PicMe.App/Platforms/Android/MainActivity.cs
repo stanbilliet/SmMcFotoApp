5c9d99cda7a89e7edf7402d8cf468a3b5391e256
stan.billiet <stanbilliet@gmail.com>
2024-10-22 21:12:04 +0200

    remove class list overhead

--- PicMe/PicMe.App/Platforms/Android/MainActivity.cs
+++ PicMe/PicMe.App/Platforms/Android/MainActivity.cs
@@ -9,8 +9,32 @@ namespace PicMe.App
 	[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 	public class MainActivity : MauiAppCompatActivity
 	{
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
 
-	}
+
+            _ = CheckAndRequestPermissionsAsync();  
+        }
+
+        public async Task CheckAndRequestPermissionsAsync()
+        {
+            var cameraStatus = await Permissions.CheckStatusAsync<Permissions.Camera>();
+            var storageStatus = await Permissions.CheckStatusAsync<Permissions.StorageWrite>();
+
+            if (cameraStatus != PermissionStatus.Granted)
+            {
+                cameraStatus = await Permissions.RequestAsync<Permissions.Camera>();
+            }
+
+            if (storageStatus != PermissionStatus.Granted)
+            {
+                storageStatus = await Permissions.RequestAsync<Permissions.StorageWrite>();
+            }
+
+
+        }
+    }
 
 }
 
